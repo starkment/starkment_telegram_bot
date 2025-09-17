b4c60af3e3f308fe59ad6ac69b30a58bbfa44bdf
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2025-09-17 04:40:16 +0100

    chore: update tg start button

--- src/bot/update/update.service.ts
+++ src/bot/update/update.service.ts
@@ -1,4 +1,39 @@
 import { Injectable } from '@nestjs/common';
+import { Start, Ctx, Action } from 'nestjs-telegraf';
+import { Context } from 'telegraf';
 
 @Injectable()
-export class UpdateService {}
+export class UpdateService {
+  @Start()
+  async start(@Ctx() ctx: Context) {
+    await ctx.reply('Welcome to Starkment Bot!\n\n', {
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: '🚀 Start',
+              callback_data: 'show_menu',
+            },
+          ],
+        ],
+      },
+    });
+  }
+
+  @Action('show_menu')
+  async showMenu(@Ctx() ctx: Context) {
+    await ctx.reply(`Starkment is global payment app` + `Choose an option:`, {
+      parse_mode: 'HTML',
+      reply_markup: {
+        inline_keyboard: [
+          [
+            {
+              text: 'send money',
+              callback_data: 'send_money',
+            },
+          ],
+        ],
+      },
+    });
+  }
+}
