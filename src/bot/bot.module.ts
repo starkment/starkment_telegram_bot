848135de5fdeafb2c7026478e1d23cbc9a54ea33
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2025-09-17 04:13:23 +0100

    chore: add telegram pkg

--- src/bot/bot.module.ts
+++ src/bot/bot.module.ts
@@ -3,9 +3,27 @@ import { BotController } from './bot.controller';
 import { BotService } from './bot.service';
 import { MessageService } from './message/message.service';
 import { UpdateService } from './update/update.service';
+import { ScheduleModule } from '@nestjs/schedule';
+import { HttpModule } from '@nestjs/axios';
+import { TelegrafModule } from 'nestjs-telegraf';
+import * as LocalSession from 'telegraf-session-local';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
+  imports: [
+    ScheduleModule.forRoot(),
+    HttpModule,
+    TelegrafModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        token: configService.get<string>('TELEGRAM_BOT_TOKEN'),
+        middlewares: [
+          new LocalSession({ database: 'session.json' }).middleware(),
+        ],
+      }),
+    }),
+  ],
   controllers: [BotController],
-  providers: [BotService, MessageService, UpdateService]
+  providers: [BotService, MessageService, UpdateService],
 })
 export class BotModule {}
