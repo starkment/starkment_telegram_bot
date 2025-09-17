0c7d62256e65588db19a8da38d797c3ed01bd410
uwezukwechibuzor <uwezukwechibuzor@gmail.com>
2025-09-17 03:09:27 +0100

    feat(wallet): add wallet module and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WalletModule } from './wallet/wallet.module';
 
 @Module({
-  imports: [],
+  imports: [WalletModule],
   controllers: [AppController],
   providers: [AppService],
 })
